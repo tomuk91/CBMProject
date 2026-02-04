2c09f92719d8d4c66cad1f7111cb0573a767120d
Tom Thornton <tthornton@prevalent.net>
2026-02-04 11:46:48 +0000

    Ensure sqlite database file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env sh
 set -e
 
+if [ "${DB_CONNECTION}" = "sqlite" ]; then
+  if [ -z "${DB_DATABASE}" ]; then
+    export DB_DATABASE="/var/www/html/database/database.sqlite"
+  fi
+  if [ ! -f "${DB_DATABASE}" ]; then
+    mkdir -p "$(dirname "${DB_DATABASE}")"
+    touch "${DB_DATABASE}"
+  fi
+  chown -R www-data:www-data "$(dirname "${DB_DATABASE}")"
+  chmod -R ug+rw "$(dirname "${DB_DATABASE}")"
+fi
+
 if [ "${RUN_MIGRATIONS}" = "true" ]; then
   php artisan migrate --force
 fi
